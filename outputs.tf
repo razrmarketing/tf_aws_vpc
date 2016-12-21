d59b1699b53d856643f0c75a4f6443dfbce5ecdb
Mike Horwath <mike@mtau.com>
2016-12-21 11:20:35 -0600

    testing a singular route table id

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,10 @@ output "public_route_table_ids" {
   value = ["${aws_route_table.public.*.id}"]
 }
 
+output "public_route_table_id" {
+  value = "${aws_route_table.public.id}"
+}
+
 output "private_route_table_ids" {
   value = ["${aws_route_table.private.*.id}"]
 }
