1af5ac87ccbac6c257a6f5ac883aedb95ad42715
Mike Horwath <mike@mtau.com>
2017-02-02 14:42:39 -0600

    a test of instance_tenancy

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
-  instance_tenancy     = "${var.instance_tenancy}"
+  instance_tenancy     = "${var.tenancy}"
 
   tags {
     Name = "${var.name}"
