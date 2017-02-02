445665e3974b41667f46e29af440003c74017190
Mike Horwath <mike@mtau.com>
2017-02-02 14:46:45 -0600

    a test of instance_tenancy

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
-  instance_tenancy     = "${var.vm_tenancy}"
+  instance_tenancy     = "dedicated"
 
   tags {
     Name = "${var.name}"
